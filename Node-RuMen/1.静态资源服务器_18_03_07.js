505c0564fa079790286243e0183cb2f044e942d1
ducen <316358726@qq.com>
2018-03-08 13:36:59 +0800

    0307静态资源服务

--- Node-RuMen/1.静态资源服务器_18_03_07.js
+++ Node-RuMen/1.静态资源服务器_18_03_07.js
@@ -1,17 +1,15 @@
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
-const conf = require('1.conf.js');
+const chalk = require('chalk');
+const conf = require('./conf/1.conf');
+const route = require('./helper/1.route');
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url);
-    fs.stat(filePath, (err, stats) => {
-
-    })
+    route(req,res,filePath);
 
 });
 
 server.listen(conf.port, conf.hostname, () => {
     const adr = `http://${conf.hostname}:${conf.port}`;
-    console.log(`Server started at ${adr}`);
-
-})
+    console.log(`Server started at ${chalk.blue(adr)}`);
+});
