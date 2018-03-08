7ee511a6df68563e0f4fb6d2de1914bbadd99569
ducen <316358726@qq.com>
2018-03-08 15:22:24 +0800

    静态资源服务器

--- Node-RuMen/helper/1.route.js
+++ Node-RuMen/helper/1.route.js
@@ -1,7 +1,14 @@
 const util = require('util');
+const  path = require('path');
 const fs = require('fs');
 const stat = util.promisify(fs.stat);
 const readdir = util.promisify(fs.readdir);
+const Handlebars = require('handlebars');
+const config = require('../conf/1.conf');
+
+const tplPath = path.join(__dirname,'../template/1.tpl.html');
+const source = fs.readFileSync(tplPath,'utf8');
+const template = Handlebars.compile(source);
 
 module.exports = async function (req, res, filePath) {
     try {
@@ -14,14 +21,14 @@ module.exports = async function (req, res, filePath) {
 
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader("Content-Type", "text/plain");
-            res.end(files.join(','));
-
-            fs.readdir(filePath, (err, files) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "text/plain");
-                res.end(files.join(','));
-            })
+            res.setHeader("Content-Type", "text/html");
+            const dir =  path.relative(config.root,filePath)
+            const data = {
+                title:path.basename(filePath),
+                dir: dir?`/${dir}`:'',
+                files
+            };
+            res.end(template(data));
 
         }
     } catch (ex) {
